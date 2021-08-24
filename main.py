31a151cd0312dd5eb5213ec30e63f422f76a4a39
Avan Tsang <88766299+paperweightcode@users.noreply.github.com>
2021-08-24 19:59:50 +0000

    Added Kanye quote command.

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import discord
 import os
+import requests
+import json
+
+
+#Retrieve secret bot token
 bkey = os.environ['bkey']
 
+#Initialise bot client
 client = discord.Client()
 
+def kanye_quote():
+  '''
+  Description: Uses REST API to fetch Kanye Quotes
+  '''
+  response = requests.get("https://api.kanye.rest").json()
+  #json_data = json.loads(response)
+  #quote = json
+  return(response)
+
+#Successful login confirmation (in console)
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
+#Bot messges
 @client.event
 async def on_message(message):
   if message.author == client.user:
       return
+  if message.content.startswith('$kanye'):
+      quote = kanye_quote()['quote']
+      await message.channel.send(f"*\"{quote}\" - Kanye*")
 
-  if message.content.startswith('$hello'):
-      await message.channel.send('Hello!')
-
+#Run bot (go online)
 client.run(bkey)
